cb9126a4560ba5b2c0163942c77f6c30ce6058c8
Paolo Rechia <paolorechia@gmail.com>
2020-06-28 12:51:15 -0300

    Find tail first successful way

--- rust/tutorials/mutablelist/src/main.rs
+++ rust/tutorials/mutablelist/src/main.rs
@@ -54,15 +54,37 @@ impl List {
         }
     }
 
-    
-    /*
-    fn _find_tail(n: &mut Option<Rc<RefCell<List>>>) -> &Option<Rc<RefCell<List>>> {
-        while n.hasNext {
-            n = &mut n.as_ref().unwrap().borrow_mut().next;
+    fn get_next(n: &Option<Rc<RefCell<List>>>) -> Option<Rc<RefCell<List>>> {
+        match n.as_ref().is_some() {
+            true => {
+                let is_some = n.as_ref().unwrap().borrow().next.is_some();
+                match is_some {
+                    true => Some(
+                        Rc::clone(n.as_ref().unwrap().borrow().next.as_ref().unwrap())
+                    ),
+                    false => None
+                }
+            }
+            false => None
         }
     }
-    */
-    
+
+    fn _find_tail(n: &Option<Rc<RefCell<List>>>) -> Option<Rc<RefCell<List>>> {
+        let mut x = Some(Rc::clone(n.as_ref().unwrap()));
+        let mut tail = Some(Rc::clone(n.as_ref().unwrap()));
+        let mut has_next = n.as_ref().is_some();
+        while has_next {
+            let u = Some(Rc::clone(&List::get_next(&x).as_ref().unwrap()));
+            println!("{:?}", u);
+            has_next = u.as_ref().unwrap().borrow().next.as_ref().is_some();
+            println!("has_next: {:?}", has_next);
+            x = u;
+            if !has_next {
+                tail = Some(Rc::clone(x.as_ref().unwrap())); 
+            }
+        }
+        tail
+    }
 }
 
 fn main () {
@@ -72,5 +94,15 @@ fn main () {
     l.push(5);
     l.push(6);
 
-//    List::_find_tail(&mut l.next);
+    let n = List::get_next(&l.next);
+    println!("{:?}", n);
+    let n2 = List::get_next(&n);
+    println!("{:?}", n2);
+    let n3 = List::get_next(&n2);
+    println!("{:?}", n3);
+    println!("{:?}", l);
+    println!("Running find tail");
+    let tail = List::_find_tail(&l.next);
+    println!("Found tail: {:?}", tail);
+    println!("{:?}", l);
 }
